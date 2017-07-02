e1d74a2f5117693f4f380c42871fcaad5f7a61b2
Chris Allen <chris@cpallen.com>
2017-07-02 08:11:35 -0700

    footer link update

--- wp-content/themes/WPBaseFoundation/footer.php
+++ wp-content/themes/WPBaseFoundation/footer.php
@@ -18,12 +18,12 @@
 			<li>
 				<a href="https://twitter.com/chrispaulallen" class="icon-twitter" target="_blank">
 					<span class="hide-text">Twitter</span>
-				</a>	
+				</a>
 			</li>
 			<li>
-				<a href="https://www.linkedin.com/profile/view?id=183690620&trk=spm_pic" class="icon-linkedin" target="_blank">
+				<a href="https://www.linkedin.com/in/chrispaulallen/" class="icon-linkedin" target="_blank">
 					<span class="hide-text">LinkedIn</span>
-				</a>	
+				</a>
 			</li>
 			<li>
 				<a href="https://github.com/Christopherallen" class="icon-github" target="_blank">
@@ -34,13 +34,13 @@
 				<a href="mailto:chris@cpallen.com" class="icon-mail" target="_blank">
 					<span class="hide-text">Email Me</span>
 				</a>
-			</li>	
+			</li>
 		</ul>
 	</div>
 	<div class="footer-copy">
 		<p class="copywrite"><?php echo date('Y');?> &copy; Chris Allen All Rights Reserved | <a href="site-map">Site Map <?php if( get_field('resume', 'options') ): ?> | <a href="<?php the_field('resume', 'options'); ?>" target="_blank">Resume</a><?php endif; ?></p>
 	</div>
-</footer> 
+</footer>
 <script type="text/javascript">
     $(document).foundation();
 </script>
